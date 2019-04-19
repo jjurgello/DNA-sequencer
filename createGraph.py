866476105523b33cc727e37c8ab45825c7a1d02e
Jennifer Steele <jenni1317@gmail.com>
2019-04-19 09:49:04 -0500

    installation of igraph successful, basic graph created

---
+++ createGraph.py
@@ -0,0 +1,18 @@
+from igraph import *
+
+# k = 3 # k-mer of the reads
+
+# # pull string from a txt file
+# readstr = "AAA AAB ABB BBB BBA"
+
+# # split the reads strings into individual reads, counting the number of reads
+# reads = readstr.split(' ', 1)
+# num_vertices = len(reads)
+
+# # split the reads into left/right k-1-mers and find common substrings
+# edge_list = 
+
+g = Graph()
+print(g)
+# g.add_vertices(num_vertices)
+# g.add_edges(edge_list)
