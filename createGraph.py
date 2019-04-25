22ef9a59d6d0a7f366e4e02e25eae9ccfb8608e0
JJ Urgello <urgello2@illinois.edu>
2019-04-24 21:22:32 -0500

    - input string formatting complete

--- createGraph.py
+++ createGraph.py
@@ -1,18 +1,43 @@
 from igraph import *
 
-# k = 3 # k-mer of the reads
+### USAGE
+# python createGraph.py 3
 
-# # pull string from a txt file
-# readstr = "AAA AAB ABB BBB BBA"
+### FORMATTING THE SEQUENCE
 
-# # split the reads strings into individual reads, counting the number of reads
-# reads = readstr.split(' ', 1)
-# num_vertices = len(reads)
+# inputs
+with open('data.txt', 'r') as sequencefile:
+    inputstr = sequencefile.read().replace('\n', '') # input string
+k = int(sys.argv[1]) # k-mer of the reads
 
-# # split the reads into left/right k-1-mers and find common substrings
-# edge_list = 
+# split the string into reads
+# here, I iterate through the input string by selecting k lengths
+reads = [inputstr[i:i+k] for i in range(0, len(inputstr), k)]
+# list of (k-1)-mer tuples for each read (left/right)
+# here, I iterate through each read and split into k-1-mers, setting left and right
+splitReads = [(read[0:k-1], read[1:]) for read in reads]
+# list of (k-1)-mers, without repeats
+# here, I iterate through each pair in splitReads, and a python set prevents repeats
+vertexLabels = list(set([read for pair in splitReads for read in pair]))
+
+# vertexLabels = ["AA", "AB", "BB", "BA"]
+# splitReads = [("AA", "AA"), ("AA", "AB"), ("AB", "BB"), ("BB", "BB"), ("BB", "BA")]
+
+### TESTING (DELETE)
+print(vertexLabels)
+print(splitReads)
+
+### CREATING THE GRAPH
+
+# g = Graph()
+# g.add_vertices(len(vertexLabels))
+# g.vs["seq"] = vertexLabels
+# for pair in splitReads:
+#     left = g.vs.select(seq_eq = pair[0])     #not sure about the runtime of this - might have to store a deparate map from vertexLabel -> vertexID
+#     right = g.vs.select(seq_eq = pair[1])
+#     g.add_edges(c(left, right))
 
 g = Graph()
+g.add_vertices(len(vertexLabels))
+g.add_edges([1,2])
 print(g)
-# g.add_vertices(num_vertices)
-# g.add_edges(edge_list)
