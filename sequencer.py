115b88cb61b1251bd4c7c025a1d8cb23972a58b0
JJ Urgello <urgello2@illinois.edu>
2019-05-06 19:41:52 -0500

    - fix implementation mistake, split spacing

--- sequencer.py
+++ sequencer.py
@@ -3,6 +3,7 @@
 
 import igraph
 import sys
+import re
 
 ### USAGE
 # python sequencer.py [k, not necessary]
@@ -10,13 +11,16 @@ import sys
 ### FORMATTING THE SEQUENCE
 
 # inputs
-with open('data.txt', 'r') as sequencefile: 
-    inputstr = sequencefile.read().replace('\n', '') # input string 
+validchars = ['A', 'T', 'C', 'G']
+with open('data.txt', 'r') as sequencefile: # set inputstr
+    inputstr = ''.join([c for c in sequencefile.read() if c in validchars])
 k = int(sys.argv[1]) if len(sys.argv) > 1 else 3 # k-mer of the reads
 
 # split the string into reads
 # here, I iterate through the input string by selecting k lengths
-reads = [inputstr[i:i+k] for i in range(0, len(inputstr), k)]
+reads = [inputstr[i:i+k] for i in range(0, len(inputstr) - k)]
+#if len(inputstr) % k != 0:
+#    reads = reads[0:len(reads)-1]
 # list of (k-1)-mer tuples for each read (left/right)
 # here, I iterate through each read and split into k-1-mers, setting left and right
 splitReads = [(read[0:k-1], read[1:]) for read in reads]
@@ -27,7 +31,7 @@ vertexLabels = list(set([read for pair in splitReads for read in pair]))
 ### CREATING THE GRAPH
 
 # create an igraph
-g = igraph.Graph(1)
+g = igraph.Graph()
 # add the vertex labels
 g.add_vertices(vertexLabels)
 # add the edges, determined by splitReads
@@ -39,3 +43,6 @@ g.add_edges(edges)
 # display the graph
 print(g)
 
+### SEQUENCING
+
+#
